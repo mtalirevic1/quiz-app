550067633a8d1eb464766152417edff7c859f7a0
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2019-03-29 19:09:13 +0100

    Adapter za ListView u klasi KvizoviAkt

---
+++ app/src/main/java/ba/unsa/etf/rma/klase/KvizoviAktAdapter.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rma.klase;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class KvizoviAktAdapter extends BaseAdapter {
+
+    private Activity aktivnost;
+    private ArrayList podaci;
+    private static LayoutInflater inflater=null;
+    public Resources res;
+
+}
