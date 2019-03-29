7b10bbc6a9917d23655b12819bc377711fa7688f
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2019-03-29 18:55:49 +0100

    Spinner i ListView u klasi KvizoviAkt, kvizovi_akt novi naziv xml datoteke za tu aktivnost

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
@@ -1,13 +1,15 @@
-package ba.unsa.etf.rma;
+package ba.unsa.etf.rma.aktivnosti;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ba.unsa.etf.rma.R;
+
+public class KvizoviAkt extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.kvizovi_akt);
     }
 }
