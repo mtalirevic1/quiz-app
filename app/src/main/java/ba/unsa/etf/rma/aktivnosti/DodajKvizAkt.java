842e745181ff6c51f7dab53374d4760a4e023ac9
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2019-04-05 00:12:08 +0200

    Razvoj DodajKvizAkt aktivnosti i dodaj_kviz_akt layout xml, adapter za listu u toj klasi

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKvizAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKvizAkt.java
@@ -1,13 +1,19 @@
 package ba.unsa.etf.rma.aktivnosti;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+
 import ba.unsa.etf.rma.R;
+import ba.unsa.etf.rma.klase.DodajKvizAdapter;
+import ba.unsa.etf.rma.klase.Pitanje;
 
 public class DodajKvizAkt extends AppCompatActivity {
     private ListView lvDodanaPitanja;
@@ -15,12 +21,24 @@ public class DodajKvizAkt extends AppCompatActivity {
     private Spinner spKategorije;
     private EditText etNaziv;
     private Button btnDodajKviz;
+    private DodajKvizAdapter adapter;
+    private ArrayList<String> kategorije;
+    private ArrayList<Pitanje> mogucaPitanja;
+    private ArrayList<Pitanje> pitanjaKviza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dodaj_kviz_akt);
         etNaziv=(EditText) findViewById(R.id.etNaziv);
+        lvDodanaPitanja=(ListView) findViewById(R.id.lvDodanaPitanja);
+        lvMogucaPitanja=(ListView) findViewById(R.id.lvMogucaPitanja);
+        spKategorije=(Spinner) findViewById(R.id.spKategorije);
+        btnDodajKviz=(Button) findViewById(R.id.btnDodajKviz);
 
+        Resources res=getResources();
+        mogucaPitanja=new ArrayList<>();
+        pitanjaKviza=new ArrayList<>();
+        adapter =new DodajKvizAdapter(this,pitanjaKviza,res);
     }
 }
---
+++ app/src/main/java/ba/unsa/etf/rma/klase/DodajKvizAdapter.java
@@ -0,0 +1,105 @@
+package ba.unsa.etf.rma.klase;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ba.unsa.etf.rma.R;
+import ba.unsa.etf.rma.aktivnosti.KvizoviAkt;
+
+public class DodajKvizAdapter extends BaseAdapter {
+    private Activity activity;
+    private ArrayList data;
+    private static LayoutInflater inflater = null;
+    public Resources res;
+    Kviz kviz = null;
+
+    public DodajKvizAdapter(Activity activity, ArrayList podaci, Resources res) {
+        this.activity = activity;
+        this.data = podaci;
+        this.res = res;
+        inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        if (data.size() <= 0)
+            return 1;
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public static class ViewHolder {
+
+        public TextView text;
+        public ImageView image;
+
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        DodajKvizAdapter.ViewHolder holder;
+
+        if (convertView == null) {
+
+            view = inflater.inflate(R.layout.element_liste, null);
+
+            holder = new DodajKvizAdapter.ViewHolder();
+            holder.text = (TextView) view.findViewById(R.id.text);
+            holder.image = (ImageView) view.findViewById(R.id.image);
+
+            view.setTag(holder);
+        } else {
+            holder = (DodajKvizAdapter.ViewHolder) view.getTag();
+        }
+
+        kviz = null;
+        kviz = (Kviz) data.get(position);
+
+        if(position!=data.size()-1) {
+            holder.text.setText(kviz.getNaziv());
+            holder.image.setImageResource(res.getIdentifier("plavikrug", "drawable", "ba.unsa.etf.rma"));
+        }
+        else{
+            holder.text.setText("Dodaj Pitanje");
+            holder.image.setImageResource(res.getIdentifier("zeleniplus", "drawable", "ba.unsa.etf.rma"));
+        }
+
+        view.setOnClickListener(new DodajKvizAdapter.OnItemClickListener(position));
+
+        return view;
+    }
+
+    private class OnItemClickListener implements View.OnClickListener {
+        private int mPosition;
+
+        OnItemClickListener(int position) {
+            mPosition = position;
+        }
+
+        @Override
+        public void onClick(View arg0) {
+            KvizoviAkt sct = (KvizoviAkt) activity;
+            sct.onItemClick(mPosition);
+        }
+    }
+}
