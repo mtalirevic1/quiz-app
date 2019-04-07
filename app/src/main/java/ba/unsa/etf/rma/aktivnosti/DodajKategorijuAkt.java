95ec87e64f2e22bb0ba2535feba12631b8fb5581
mtalirevic1 <mtalirevic1@etf.unsa.ba>
2019-04-07 18:46:32 +0200

    DodajKategorijuAktivnost i xml, xml za ikone

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKategorijuAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKategorijuAkt.java
@@ -1,15 +1,21 @@
 package ba.unsa.etf.rma.aktivnosti;
 
-import android.graphics.drawable.Icon;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.maltaisn.icondialog.Icon;
 import com.maltaisn.icondialog.IconDialog;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 import ba.unsa.etf.rma.R;
+import ba.unsa.etf.rma.klase.Kategorija;
 
 public class DodajKategorijuAkt extends AppCompatActivity implements IconDialog.Callback {
     private Icon[] selectedIcons;
@@ -17,12 +23,15 @@ public class DodajKategorijuAkt extends AppCompatActivity implements IconDialog.
     private EditText etIkona;
     private Button btnDodajIkonu;
     private Button btnDodajKategoriju;
-
+    private ArrayList<Kategorija> kategorije;
 
     @Override
     protected void onCreate(Bundle state) {
         super.onCreate(state);
         setContentView(R.layout.dodaj_kategoriju_akt);
+
+        Bundle bundle=getIntent().getExtras();
+        kategorije=bundle.getParcelableArrayList("kategorije");
 
         final IconDialog iconDialog = new IconDialog();
         etNaziv = (EditText) findViewById(R.id.etNaziv);
@@ -30,25 +39,73 @@ public class DodajKategorijuAkt extends AppCompatActivity implements IconDialog.
         btnDodajIkonu = (Button) findViewById(R.id.btnDodajIkonu);
         btnDodajKategoriju = (Button) findViewById(R.id.btnDodajKategoriju);
 
+        etIkona.setFocusable(false);
+
         btnDodajIkonu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //  iconDialog.setSelectedIcons(selectedIcons);
+                iconDialog.setSelectedIcons(selectedIcons);
                 iconDialog.show(getSupportFragmentManager(), "icon_dialog");
-                iconDialog.getIconFilter();
-                etIkona.setText(Integer.toString(iconDialog.getId())); // nije jos dobro
+            }
+        });
+
+        btnDodajKategoriju.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!imaIme()) {
+                    etNaziv.setBackgroundColor(getResources().getColor(R.color.red));
+                } else{
+                    etNaziv.setBackgroundColor(getResources().getColor(R.color.colorLabel1));
+                }
+
+                if (!imaId()) {
+                    etIkona.setBackgroundColor(getResources().getColor(R.color.red));
+                } else {
+                    etIkona.setBackgroundColor(getResources().getColor(R.color.colorLabel1));
+                }
+
+                if(imaIme() && imaId() && !imaKategoriju(etIkona.getText().toString())){
+                    Intent returnIntent = new Intent();
+                    String s=etNaziv.getText().toString();
+                    returnIntent.putExtra("ikona",selectedIcons[0].getId());
+                    returnIntent.putExtra("kategorija",s);
+                    setResult(RESULT_OK, returnIntent);
+                    finish();
+                }
             }
         });
 
     }
 
     @Override
-    public void onIconDialogIconsSelected(com.maltaisn.icondialog.Icon[] icons) {
-
+    public void onIconDialogIconsSelected(Icon[] icons) {
+        selectedIcons=icons;
+        for(int i=0;i<selectedIcons.length;i++) {
+            etIkona.setText(""+selectedIcons[i].getId());
+        }
     }
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
+    public boolean imaIme() {
+        String s = etNaziv.getText().toString();
+        if (!s.equals(""))
+            return true;
+        return false;
     }
+
+    public boolean imaId() {
+        String s = etIkona.getText().toString();
+        if (!s.equals(""))
+            return true;
+        return false;
+    }
+
+    public boolean imaKategoriju(String s){
+        for(Kategorija k: kategorije){
+            if(k.getId().equals(s)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
